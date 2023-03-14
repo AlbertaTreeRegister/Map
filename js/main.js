feb0852b0d915a3cf429a584d1573d43b4ab15d8
David Desjarlais <ddesjarlais5@gmail.com>
2023-03-14 00:54:41 -0600

    add basic leaderboard, fix map center point

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const mobile = window.matchMedia("(max-width: 767px)").matches;
 let map = '';
-let markers = [];
+let treeLayer = '';
 let treeRecords = [];
 let nominating = false;
 let displayFields = [
@@ -52,9 +52,11 @@ async function fetchTreeRecords() {
 }
 
 function addTreeMarkers() {
+  let treeFeatures = [];
+
   // Add markers to the map
   treeRecords.forEach(function(record) {
-    let marker = new ol.Feature({
+    let treeFeature = new ol.Feature({
       geometry: new ol.geom.Point(
         ol.proj.fromLonLat([
           record.fields.Longitude,
@@ -62,12 +64,13 @@ function addTreeMarkers() {
         ])
       )
     });
+    treeFeature.setId(record.id);
 
     for (let propertyName in record.fields) {
-      marker.set(propertyName, record.fields[propertyName]);
+      treeFeature.set(propertyName, record.fields[propertyName]);
     }
 
-    markers.push(marker);
+    treeFeatures.push(treeFeature);
   });
 
   let iconStyles = {
@@ -75,25 +78,31 @@ function addTreeMarkers() {
       image: new ol.style.Icon({
         src: 'img/tree.png',
         anchor: [0.5, 1]
+      }),
+      text: new ol.style.Text({
+        font: '14px Calibri,sans-serif',
+        fill: new ol.style.Fill({ color: '#000' }),
+        stroke: new ol.style.Stroke({
+          color: '#fff', width: 3
+        }),
+        offsetY: 18,
       })
     }),
     'Grove of Trees': new ol.style.Style({
       image: new ol.style.Icon({
         src: 'img/forest.png',
         anchor: [0.5, 1],
+      }),
+      text: new ol.style.Text({
+        font: '14px Calibri,sans-serif',
+        fill: new ol.style.Fill({ color: '#000' }),
+        stroke: new ol.style.Stroke({
+          color: '#fff', width: 3
+        }),
+        offsetY: 18,
       })
     })
   };
-
-  let treeMarkerLayer = new ol.layer.Vector({
-    source: new ol.source.Vector({
-      features: markers
-    }),
-    style: function (feature) {
-      let iconStyle = iconStyles[feature.get('Category')];
-      return iconStyle ? iconStyle : iconStyles["Individual Tree"];
-    }
-  });
 
   let baseTileLayer = new ol.layer.Tile({
     source: new ol.source.OSM({
@@ -101,14 +110,33 @@ function addTreeMarkers() {
     })
   });
 
+  treeLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      features: treeFeatures
+    }),
+    style: function (feature) {
+      let iconStyle = iconStyles[feature.get('Category')];
+      let style = iconStyle ? iconStyle : iconStyles["Individual Tree"];
+      if(map.getView().getZoom() > 14) {
+        style.getText().setText(feature.get('Tree Name'));
+      }
+      else {
+        style.getText().setText('');
+      }
+      return style;
+    }
+  });
+
   // Set up the map
   map = new ol.Map({
     target: 'map',
-    layers: [baseTileLayer, treeMarkerLayer],
+    layers: [baseTileLayer, treeLayer],
     view: new ol.View({
       //center: ol.proj.fromLonLat([-114.337082, 54.678073]),
       zoom: 6, // Set an appropriate zoom level for your data
-      enableRotation: false
+      enableRotation: false,
+      maxZoom: 18,
+      minZoom: 4
     }),
     controls: []
   });
@@ -161,142 +189,7 @@ function setupMapFunctions() {
       let feature = map.forEachFeatureAtPixel(event.pixel, function (feature) {
         return feature;
       });
-      if (feature) {
-        let html = '';
-        let name = feature.get('Tree Name');
-        html += `<p class="treeName"><strong>${name}</strong></p>`;
-        let description = feature.get('Description');
-        if (description) {
-          html += `<p>${description}</p>`;
-        }
-
-        displayFields.forEach(function(field){
-          let fieldValue = feature.get(field);
-          if(fieldValue) {
-            html += `<p><strong>${field}:</strong> ${fieldValue}</p>`;
-          }
-        });
-
-        // Update Info Panel with Tree Information
-        const infoPanel = document.getElementById('infoPanel-content');
-        infoPanel.innerHTML = html;
-
-        // Add Google Maps button to bottom of Tree Info
-        let googleMapsButton = document.createElement('button');
-        googleMapsButton.style.border = "none";
-        googleMapsButton.style.background = "none";
-        googleMapsButton.title = "Open in Google Maps";
-        let googleMapsIcon = '<img id="googleMapsIcon" src="img/google-maps-old.svg" style="width: 48px; height: 48px">';
-        googleMapsButton.innerHTML = googleMapsIcon;
-        //googleMapsButton.appendChild(googleMapsIcon);
-        //googleMapsButton.innerHTML = 'Open in Google Maps';
-        googleMapsButton.addEventListener('click', function () {
-          let latitude = feature.get('Latitude'); // replace with the latitude of the location
-          let longitude = feature.get('Longitude'); // replace with the longitude of the location
-          let url = 'https://www.google.com/maps/search/?api=1&query=' + latitude + '%2C' + longitude;
-          window.open(url);
-        });
-
-        infoPanel.appendChild(googleMapsButton);
-
-        //set up image carousel
-
-        // reset carousel
-        const carouselIndicators = document.querySelector(".carousel-indicators");
-        carouselIndicators.innerHTML = "";
-        const carouselInner = document.querySelector(".carousel-inner");
-        carouselInner.innerHTML = "";
-
-        let photos = feature.get('Photo');
-        if (photos) {
-          photos.forEach((image, index) => {
-            // create carousel indicator
-            const indicator = document.createElement("button");
-            indicator.setAttribute("data-bs-target", "#treeCarousel");
-            indicator.setAttribute("data-bs-slide-to", index);
-            indicator.setAttribute("aria-label", 'Slide ' + (index + 1));
-
-            // create carousel item
-            const item = document.createElement("div");
-            item.classList.add("carousel-item");
-
-            // create image element
-            const img = document.createElement("img");
-            img.classList.add("d-block", "w-100");
-            img.src = image.url;
-
-            if (index === 0) {
-              indicator.classList.add("active");
-              item.classList.add("active");
-              img.addEventListener('load', function() {
-                scrollInfoPanelUp();
-              });
-            }
-
-            // add image to item and item to inner carousel
-            carouselIndicators.appendChild(indicator);
-            item.appendChild(img);
-            carouselInner.appendChild(item);
-          });
-
-          const carouselNextBtn = document.querySelector(".carousel-control-next");
-          const carouselPrevBtn = document.querySelector(".carousel-control-prev");
-          if (photos.length === 1) {
-            carouselIndicators.style.display = "none";
-            carouselNextBtn.style.display = "none";
-            carouselPrevBtn.style.display = "none";
-          } else {
-            carouselIndicators.style.display = "";
-            carouselNextBtn.style.display = "";
-            carouselPrevBtn.style.display = "";
-          }
-
-          // Click to Fullscreen images
-          const carouselImages = document.querySelectorAll("#treeCarousel .carousel-item img");
-
-          if (document.fullscreenEnabled) {
-            carouselImages.forEach((image) => {
-              image.style.cursor = 'zoom-in';
-              //image.style.maxHeight = '600px';
-              image.addEventListener('click', function () {
-                if (!document.fullscreenElement) {
-                  if (image.requestFullscreen) {
-                    image.requestFullscreen();
-                  } else if (image.webkitRequestFullscreen) {
-                    image.webkitRequestFullscreen();
-                  }
-                  image.style.cursor = 'zoom-out';
-                } else {
-                  document.exitFullscreen();
-                  image.style.cursor = 'zoom-in';
-                }
-              });
-
-              document.addEventListener('fullscreenchange', function () {
-                if (document.fullscreenElement) {
-                  image.style.width = '100%';
-                  image.style.height = '100%';
-                  image.style.position = 'fixed';
-                  image.style.top = '0';
-                  image.style.left = '0';
-                  image.style.zIndex = '9999';
-                } else {
-                  image.style.width = '';
-                  image.style.height = '';
-                  image.style.position = '';
-                  image.style.top = '';
-                  image.style.left = '';
-                  image.style.zIndex = '';
-                }
-              });
-            });
-          }
-          const carousel = new bootstrap.Carousel('#treeCarousel');
-        }
-        else {
-          scrollInfoPanelUp();
-        }
-      }
+      showTreeInfo(feature);
     }
   });
 
@@ -324,6 +217,145 @@ function setupMapFunctions() {
         tooltipOverlay.getElement().style.display = 'none';
       }
     });
+  }
+}
+
+function showTreeInfo(feature) {
+  if (feature) {
+    let html = '';
+    let name = feature.get('Tree Name');
+    html += `<p class="treeName"><strong>${name}</strong></p>`;
+    let description = feature.get('Description');
+    if (description) {
+      html += `<p>${description}</p>`;
+    }
+
+    displayFields.forEach(function(field){
+      let fieldValue = feature.get(field);
+      if(fieldValue) {
+        html += `<p><strong>${field}:</strong> ${fieldValue}</p>`;
+      }
+    });
+
+    // Update Info Panel with Tree Information
+    const infoPanel = document.getElementById('infoPanel-content');
+    infoPanel.innerHTML = html;
+
+    // Add Google Maps button to bottom of Tree Info
+    let googleMapsButton = document.createElement('button');
+    googleMapsButton.style.border = "none";
+    googleMapsButton.style.background = "none";
+    googleMapsButton.title = "Open in Google Maps";
+    let googleMapsIcon = '<img id="googleMapsIcon" src="img/google-maps-old.svg" style="width: 48px; height: 48px">';
+    googleMapsButton.innerHTML = googleMapsIcon;
+    //googleMapsButton.appendChild(googleMapsIcon);
+    //googleMapsButton.innerHTML = 'Open in Google Maps';
+    googleMapsButton.addEventListener('click', function () {
+      let latitude = feature.get('Latitude'); // replace with the latitude of the location
+      let longitude = feature.get('Longitude'); // replace with the longitude of the location
+      let url = 'https://www.google.com/maps/search/?api=1&query=' + latitude + '%2C' + longitude;
+      window.open(url);
+    });
+
+    infoPanel.appendChild(googleMapsButton);
+
+    //set up image carousel
+
+    // reset carousel
+    const carouselIndicators = document.querySelector(".carousel-indicators");
+    carouselIndicators.innerHTML = "";
+    const carouselInner = document.querySelector(".carousel-inner");
+    carouselInner.innerHTML = "";
+
+    let photos = feature.get('Photo');
+    if (photos) {
+      photos.forEach((image, index) => {
+        // create carousel indicator
+        const indicator = document.createElement("button");
+        indicator.setAttribute("data-bs-target", "#treeCarousel");
+        indicator.setAttribute("data-bs-slide-to", index);
+        indicator.setAttribute("aria-label", 'Slide ' + (index + 1));
+
+        // create carousel item
+        const item = document.createElement("div");
+        item.classList.add("carousel-item");
+
+        // create image element
+        const img = document.createElement("img");
+        img.classList.add("d-block", "w-100");
+        img.src = image.url;
+
+        if (index === 0) {
+          indicator.classList.add("active");
+          item.classList.add("active");
+          img.addEventListener('load', function() {
+            scrollInfoPanelUp();
+          });
+        }
+
+        // add image to item and item to inner carousel
+        carouselIndicators.appendChild(indicator);
+        item.appendChild(img);
+        carouselInner.appendChild(item);
+      });
+
+      const carouselNextBtn = document.querySelector(".carousel-control-next");
+      const carouselPrevBtn = document.querySelector(".carousel-control-prev");
+      if (photos.length === 1) {
+        carouselIndicators.style.display = "none";
+        carouselNextBtn.style.display = "none";
+        carouselPrevBtn.style.display = "none";
+      } else {
+        carouselIndicators.style.display = "";
+        carouselNextBtn.style.display = "";
+        carouselPrevBtn.style.display = "";
+      }
+
+      // Click to Fullscreen images
+      const carouselImages = document.querySelectorAll("#treeCarousel .carousel-item img");
+
+      if (document.fullscreenEnabled) {
+        carouselImages.forEach((image) => {
+          image.style.cursor = 'zoom-in';
+          //image.style.maxHeight = '600px';
+          image.addEventListener('click', function () {
+            if (!document.fullscreenElement) {
+              if (image.requestFullscreen) {
+                image.requestFullscreen();
+              } else if (image.webkitRequestFullscreen) {
+                image.webkitRequestFullscreen();
+              }
+              image.style.cursor = 'zoom-out';
+            } else {
+              document.exitFullscreen();
+              image.style.cursor = 'zoom-in';
+            }
+          });
+
+          document.addEventListener('fullscreenchange', function () {
+            if (document.fullscreenElement) {
+              image.style.width = '100%';
+              image.style.height = '100%';
+              image.style.position = 'fixed';
+              image.style.top = '0';
+              image.style.left = '0';
+              image.style.zIndex = '9999';
+            } else {
+              image.style.width = '';
+              image.style.height = '';
+              image.style.position = '';
+              image.style.top = '';
+              image.style.left = '';
+              image.style.zIndex = '';
+            }
+          });
+        });
+      }
+      const carousel = new bootstrap.Carousel('#treeCarousel');
+    }
+    else {
+      scrollInfoPanelUp();
+    }
   }
 }
 
@@ -361,6 +393,69 @@ function disableNominating()
   const mapElement = document.getElementById('map');
   mapElement.style.cursor = 'auto';
   document.getElementById('nominateBtn').textContent = 'Nominate a Tree';
+}
+
+function buildLeaderboard() {
+  // Create the table element and add it to the container
+  let tableElement = document.createElement('table');
+  tableElement.classList.add('table');
+
+  // Create the table header element and add it to the table
+  let tableHeaderElement = document.createElement('thead');
+  let tableHeaderRowElement = document.createElement('tr');
+  let nameHeaderElement = document.createElement('th');
+  nameHeaderElement.innerText = 'Name';
+  let scoreHeaderElement = document.createElement('th');
+  scoreHeaderElement.innerText = 'Score';
+  tableHeaderRowElement.appendChild(nameHeaderElement);
+  tableHeaderRowElement.appendChild(scoreHeaderElement);
+  tableHeaderElement.appendChild(tableHeaderRowElement);
+  tableElement.appendChild(tableHeaderElement);
+
+  // Create the table body element and add it to the table
+  let tableBodyElement = document.createElement('tbody');
+  tableElement.appendChild(tableBodyElement);
+
+  treeRecords.sort(function(a,b){
+    return b.fields["Species Score"] - a.fields["Species Score"];
+  });
+
+  let topTrees = treeRecords.slice(0,10);
+
+  topTrees.forEach(function(tree) {
+    // Create a new row element
+    let rowElement = document.createElement('tr');
+    rowElement.setAttribute('data-feature-id', tree.id);
+
+    // Create new cell elements for each field and add them to the row
+    let nameCell = document.createElement('td');
+    nameCell.innerText = tree.fields["Tree Name"];
+    rowElement.appendChild(nameCell);
+
+    let scoreCell = document.createElement('td');
+    scoreCell.innerText = Number(tree.fields["Species Score"].toPrecision(4));
+    rowElement.appendChild(scoreCell);
+
+    // Add the row to the table body
+    tableBodyElement.appendChild(rowElement);
+
+    // Add a click event listener to each table row
+    rowElement.addEventListener('click', function(event) {
+      // Zoom the map to the corresponding feature
+      let feature = treeLayer.getSource().getFeatureById(tree.id);
+      let extent = feature.getGeometry().getExtent();
+      map.getView().fit(extent, { duration: 2000 });
+
+      // Simulate a click event on the feature
+      showTreeInfo(feature);
+    });
+  });
+  // Update Info Panel with Tree Information
+  const infoPanel = document.getElementById('infoPanel-content');
+  infoPanel.innerHTML = `<p class="treeName"><strong>Species Score Leaders</strong></p>`;
+  infoPanel.appendChild(tableElement);
+
+  scrollInfoPanelUp();
 }
 
 // hide carousel controls by default
